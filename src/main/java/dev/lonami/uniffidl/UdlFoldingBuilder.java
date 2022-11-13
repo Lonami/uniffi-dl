818b7c639bb3e7a86b8fbd7c022e782d4747159c
Lonami Exo <totufals@hotmail.com>
2022-11-13 17:31:47 +0100

    Update folder to work on all definitions

--- src/main/java/dev/lonami/uniffidl/UdlFoldingBuilder.java
+++ src/main/java/dev/lonami/uniffidl/UdlFoldingBuilder.java
@@ -8,7 +8,9 @@ import com.intellij.openapi.editor.FoldingGroup;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.util.PsiTreeUtil;
+import dev.lonami.uniffidl.psi.UdlDefinition;
 import dev.lonami.uniffidl.psi.UdlDictionary;
+import dev.lonami.uniffidl.psi.UdlNamespace;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -21,9 +23,9 @@ public class UdlFoldingBuilder extends FoldingBuilderEx implements DumbAware {
     public FoldingDescriptor @NotNull [] buildFoldRegions(@NotNull PsiElement root, @NotNull Document document, boolean quick) {
         List<FoldingDescriptor> descriptors = new ArrayList<>();
 
-        Collection<UdlDictionary> dictionaries = PsiTreeUtil.findChildrenOfType(root, UdlDictionary.class);
-        for (final UdlDictionary dictionary : dictionaries) {
-            descriptors.add(new FoldingDescriptor(dictionary.getNode(), dictionary.getTextRange()));
+        Collection<UdlDefinition> definitions = PsiTreeUtil.findChildrenOfType(root, UdlDefinition.class);
+        for (final UdlDefinition definition : definitions) {
+            descriptors.add(new FoldingDescriptor(definition.getNode(), definition.getTextRange()));
         }
 
         return descriptors.toArray(new FoldingDescriptor[0]);
@@ -32,11 +34,27 @@ public class UdlFoldingBuilder extends FoldingBuilderEx implements DumbAware {
     @Override
     public @Nullable String getPlaceholderText(@NotNull ASTNode node) {
         String result = "...";
-        if (node.getPsi() instanceof UdlDictionary) {
-            UdlDictionary dictionary = (UdlDictionary) node.getPsi();
-            String name = dictionary.getName();
-            if (name != null) {
-                result = "dictionary " + name + " { ... }";
+        if (node.getPsi() instanceof UdlDefinition) {
+            UdlDefinition definition = (UdlDefinition) node.getPsi();
+            if (definition.getCallbackOrInterfaceOrMixin() != null) {
+                if (definition.getCallbackOrInterfaceOrMixin().getInterfaceOrMixin() != null) {
+                    result = "interface ...";
+                }
+            } else if (definition.getNamespace() != null) {
+                result = "namespace ...";
+            } else if (definition.getPartial() != null) {
+                result = "partial ...";
+            } else if (definition.getDictionary() != null) {
+                String name = definition.getDictionary().getName();
+                if (name != null) {
+                    result = "dictionary " + name + " { ... }";
+                }
+            } else if (definition.getEnum() != null) {
+                result = "enum ...";
+            } else if (definition.getTypedef() != null) {
+                result = "typedef ...";
+            } else if (definition.getIncludesStatement() != null) {
+                result = "... includes ...";
             }
         }
         return result;
