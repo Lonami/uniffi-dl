dc396a567a3f0d763195f868a892c1b90340e34a
Lonami Exo <totufals@hotmail.com>
2022-11-19 13:06:44 +0100

    Implement a bad formatting model

---
+++ src/main/java/dev/lonami/uniffidl/UdlBlock.java
@@ -0,0 +1,50 @@
+package dev.lonami.uniffidl;
+
+import com.intellij.formatting.*;
+import com.intellij.lang.ASTNode;
+import com.intellij.psi.TokenType;
+import com.intellij.psi.formatter.common.AbstractBlock;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UdlBlock extends AbstractBlock {
+    private final SpacingBuilder spacingBuilder;
+
+    protected UdlBlock(@NotNull ASTNode node, @Nullable Wrap wrap, @Nullable Alignment alignment, SpacingBuilder spacingBuilder) {
+        super(node, wrap, alignment);
+        this.spacingBuilder = spacingBuilder;
+    }
+
+    @Override
+    protected List<Block> buildChildren() {
+        List<Block> blocks = new ArrayList<>();
+        ASTNode child = myNode.getFirstChildNode();
+        while (child != null) {
+            if (child.getElementType() != TokenType.WHITE_SPACE) {
+                Block block = new UdlBlock(child, Wrap.createWrap(WrapType.NONE, false),
+                        Alignment.createAlignment(), spacingBuilder);
+                blocks.add(block);
+            }
+            child = child.getTreeNext();
+        }
+        return blocks;
+    }
+
+    @Override
+    public Indent getIndent() {
+        return Indent.getNoneIndent();
+    }
+
+    @Override
+    public @Nullable Spacing getSpacing(@Nullable Block child1, @NotNull Block child2) {
+        return spacingBuilder.getSpacing(this, child1, child2);
+    }
+
+    @Override
+    public boolean isLeaf() {
+        return myNode.getFirstChildNode() == null;
+    }
+}
---
+++ src/main/java/dev/lonami/uniffidl/UdlFormattingModelBuilder.java
@@ -0,0 +1,27 @@
+package dev.lonami.uniffidl;
+
+import com.intellij.formatting.*;
+import com.intellij.psi.codeStyle.CodeStyleSettings;
+import dev.lonami.uniffidl.psi.UdlTypes;
+import org.jetbrains.annotations.NotNull;
+
+public class UdlFormattingModelBuilder implements FormattingModelBuilder {
+    private static SpacingBuilder createSpaceBuilder(CodeStyleSettings settings) {
+        return new SpacingBuilder(settings, UdlLanguage.INSTANCE)
+                .before(UdlTypes.OP_OPEN_BRACE)
+                .spaceIf(settings.getCommonSettings(UdlLanguage.INSTANCE.getID()).SPACE_BEFORE_CLASS_LBRACE)
+                .before(UdlTypes.DEFINITION)
+                .none();
+    }
+
+    @Override
+    public @NotNull FormattingModel createModel(@NotNull FormattingContext formattingContext) {
+        final CodeStyleSettings codeStyleSettings = formattingContext.getCodeStyleSettings();
+        return FormattingModelProvider.createFormattingModelForPsiFile(formattingContext.getContainingFile(),
+                new UdlBlock(formattingContext.getNode(),
+                        Wrap.createWrap(WrapType.NONE, false),
+                        Alignment.createAlignment(),
+                        createSpaceBuilder(codeStyleSettings)),
+                codeStyleSettings);
+    }
+}
