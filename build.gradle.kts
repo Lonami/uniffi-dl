3d5b37254a4d03ccd284e5d6af1f1c0e0819fc04
Ovi Trif <ovitrif@proton.me>
2024-08-28 13:20:22 +0200

    Support IntelliJ 2024.2+ (242+)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,44 +1,56 @@
 plugins {
     id("java")
-    id("org.jetbrains.intellij") version "1.13.3"
+    id("org.jetbrains.intellij.platform") version "2.0.1"
 }
 
 group = "dev.lonami"
-version = "0.3.2"
+version = "0.3.3"
 
 repositories {
     mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
+}
+
+dependencies {
+    intellijPlatform {
+        val type = org.jetbrains.intellij.platform.gradle.IntelliJPlatformType.IntellijIdeaCommunity
+        val version = providers.gradleProperty("ideaVersion")
+        create(type, version)
+
+        instrumentationTools()
+        // plugins(providers.gradleProperty("platformPlugins").map { it.split(',') })
+        // bundledPlugins(providers.gradleProperty("platformBundledPlugins").map { it.split(',') })
+    }
 }
 
 // Configure Gradle IntelliJ Plugin
-// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
-intellij {
-    version.set("2022.3")
-    type.set("IC") // Target IDE Platform
-
-    plugins.set(listOf(/* Plugin Dependencies */))
+// Read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin.html
+intellijPlatform {
+    pluginConfiguration {
+        version = "${rootProject.version}"
+        ideaVersion {
+            sinceBuild.set(providers.gradleProperty("sinceBuild"))
+            untilBuild.set(providers.gradleProperty("untilBuild"))
+        }
+    }
+    signing {
+        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
+        privateKey.set(System.getenv("PRIVATE_KEY"))
+        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+    }
+    publishing {
+        token.set(System.getenv("PUBLISH_TOKEN"))
+    }
+    buildSearchableOptions  = false
 }
 
 tasks {
     // Set the JVM compatibility versions
     withType<JavaCompile> {
-        sourceCompatibility = "17"
-        targetCompatibility = "17"
-    }
-
-    patchPluginXml {
-        sinceBuild.set("223")
-        untilBuild.set("231.*")
-    }
-
-    signPlugin {
-        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
-        privateKey.set(System.getenv("PRIVATE_KEY"))
-        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
-    }
-
-    publishPlugin {
-        token.set(System.getenv("PUBLISH_TOKEN"))
+        sourceCompatibility = "21"
+        targetCompatibility = "21"
     }
 }
 
